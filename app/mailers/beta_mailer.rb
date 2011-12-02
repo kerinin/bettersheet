6806aef94166321f8825f0d7badc54dbb419d7ce
Ryan Michael <kerinin@gmail.com>
2011-12-01 19:42:47 -0600

    notifying myself when people sign up or are referred

--- app/mailers/beta_mailer.rb
+++ app/mailers/beta_mailer.rb
@@ -3,10 +3,12 @@ class BetaMailer < ActionMailer::Base
   
   def sign_up(invite)
     mail(:to => invite.email, :subject => "Thanks for registering with BetterSheet!")
+    mail(:to => 'kerinin@gmail.com', :subject => "#{invite.email} signed up for BetterSheet!")
   end
   
   def recommend(invite)
     @referrer = invite.referrer
     mail(:to => invite.email, :subject => "#{@referrer} invited you to try out BetterSheet; cost management that's better than spreadsheets")
+    mail(:to => 'kerinin@gmail.com', :subject => "#{referrer} just referred #{invite.email} to BetterSheet!")
   end
 end
