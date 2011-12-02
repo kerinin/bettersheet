67b79befed1bc2cfaf01fb907a00fa7cd7ee13de
Ryan Michael <kerinin@gmail.com>
2011-12-02 16:00:14 -0600

    removing redis from test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,5 +37,5 @@ Bettersheet::Application.configure do
   # Print deprecation notices to the stderr
   config.active_support.deprecation = :stderr
   
-  ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
+  #ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
 end
