729f4c230f6a2649ad0dfc2bf1e7aead0e2398ab
Ryan Michael <kerinin@gmail.com>
2011-12-02 16:14:28 -0600

    more

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,7 +2,9 @@ unless Rails.env.production?
   ENV["REDISTOGO_URL"] = 'redis://localhost:6379'
 end
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+unless ENV["REDISTOGO_URL"].nil?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
 
-Split.redis = ENV["REDISTOGO_URL"]
+  Split.redis = ENV["REDISTOGO_URL"]
+end
